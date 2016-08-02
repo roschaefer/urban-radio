291ed108ecc39932525d71c24ca02d802d017c4d
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-08-02 22:08:28 +0200

    copy/paste code from leaflet js tutorial

---
+++ urban-radio/script.js
@@ -0,0 +1,12 @@
+var current_location = [52.39241,13.11978]
+var mymap = L.map('mapid').setView(current_location, 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+  '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+  'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  id: 'mapbox.streets'
+}).addTo(mymap);
+
+
+
